aa5f9db466cd2261bcd75a767d2dd2a2d71fe2de
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-02-21 13:43:50 +0300

    done

--- src/stores/recipeStore.js
+++ src/stores/recipeStore.js
@@ -1,4 +1,5 @@
 import { makeAutoObservable } from "mobx";
+import ingredientStore from "./ingredientStore";
 import instance from "./instance";
 
 class RecipeStore {
@@ -19,13 +20,43 @@ class RecipeStore {
 
   addRecipe = async (newRecipe, categoryId) => {
     try {
+      let dataOfRec = [];
       const formData = new FormData();
       for (const key in newRecipe) formData.append(key, newRecipe[key]);
+      newRecipe.ing
+        .trim()
+        .split(",")
+        .forEach(async (i) => {
+          dataOfRec.push({
+            name: i,
+            image: "http://localhost:8000/media/1645389851933IMG_0272.jpg",
+          });
 
+          const foundIng = ingredientStore.ingredients.find(
+            (ingre) => ingre.name === i
+          );
+          if (!foundIng) {
+            const ig = ingredientStore.addIngredient({
+              name: i,
+              image: "http://localhost:8000/media/1645389851933IMG_0272.jpg",
+            });
+          } else {
+          }
+        });
       const response = await instance.post(
         `/category/${categoryId}/recipe`,
         formData
       );
+      console.log(
+        "🚀 ~ file: recipeStore.js ~ line 51 ~ RecipeStore ~ addRecipe= ~ response.data.ingredients",
+        response.data.ingredients
+      );
+      console.log(
+        "🚀 ~ file: recipeStore.js ~ line 52 ~ RecipeStore ~ addRecipe= ~ dataOfRec",
+        dataOfRec
+      );
+      response.data.ingredients = [...dataOfRec];
+
       this.recipes.push(response.data);
     } catch (error) {
       console.log(
